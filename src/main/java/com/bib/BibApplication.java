b1dd490a1b96c268cbd0d50ae3c1caa7f639873a
Nico <40561468+ni920@users.noreply.github.com>
2022-05-20 10:20:20 +0200

    Initial Commit

---
+++ src/main/java/com/bib/BibApplication.java
@@ -0,0 +1,13 @@
+package com.bib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bib/BibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
